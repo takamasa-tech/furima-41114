991d5e12e17cb2bb01f17924f2f0a9cded49bcc7
takamasa-tech <g1012075@icloud.com>
2024-08-21 17:48:26 +0900

    ユーザー管理機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_19_214732) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_21_083807) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,6 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_19_214732) do
     t.date "birthday", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
