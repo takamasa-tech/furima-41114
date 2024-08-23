5e6533234a0887235c320e1a5d5d87638683940a
takamasa-tech <g1012075@icloud.com>
2024-08-23 14:40:30 +0900

    修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合で入力してください' }
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name, presence: true, format: { with: /\\A[ぁ-んァ-ヶ一-龥々ー]+\\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: /\\A[ぁ-んァ-ヶ一-龥々ー]+\\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい' }
   validates :birthday, presence: true
--- db/migrate/20240821081635_add_name_to_users.rb
+++ db/migrate/20240821081635_add_name_to_users.rb
@@ -4,5 +4,7 @@ class AddNameToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :first_name, :string
     add_column :users, :last_name_kana, :string
     add_column :users, :first_name_kana, :string
+    add_column :users, :nickname, :string
+    add_column :users, :birthday, :date
   end
 end
