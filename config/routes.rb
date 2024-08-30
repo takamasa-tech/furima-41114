7d881f0bf1dd7a87c638f741e47fc2047351a3ad
takamasa-tech <g1012075@icloud.com>
2024-08-30 13:00:08 +0900

    商品詳細表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:new, :create, :show] do
+  resources :items, only: [:new, :create, :show, :edit] do
     resources :purchases, only: [:index ,:create]
   end
 end
