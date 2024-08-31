7b25cf920ae8831c1516cf6fb25df15a7bf586a0
takamasa-tech <g1012075@icloud.com>
2024-08-31 11:44:43 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,11 +24,34 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = current_user.items.find(params[:id])
+  end
+
+  def update
+    @item = current_user.items.find(params[:id])
+
+    if @item.update(item_params)
+      flash[:notice] = '商品情報が更新されました'
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :condition_id,
                                  :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :image, :user_id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
