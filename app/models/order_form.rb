a5b0b33a0175f234afe60df0f622ca5750c0237f
takamasa-tech <g1012075@icloud.com>
2024-09-07 12:47:44 +0900

    Formオブジェクトを作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,19 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :price, :token, :postal_code, :prefecture_id, :city, :house_number, :building_name,
+                :phone_number
+
+  # バリデーションの定義
+  validates :user_id, :item_id, presence: true
+  validates :token, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
+  # 保存のためのメソッド
+  def save
+    # データの保存処理を書く
+  end
+end
