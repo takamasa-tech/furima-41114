415453082b12cc37ae3ec0641b3036d669263a3c
takamasa-tech <g1012075@icloud.com>
2024-09-18 16:27:53 +0900

    ShippingAddressモデル修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,9 +1,3 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
-
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 end
