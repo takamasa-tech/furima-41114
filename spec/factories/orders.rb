a3acad29e90b6ca60149459a441ce7e78c382578
takamasa-tech <g1012075@icloud.com>
2024-09-18 18:53:37 +0900

    35

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :order do
-    user { create(:user) } # Userとの関連付け
-    item { create(:item) } # Itemとの関連付け
-    credit_card { '1234567890123456' } # 16桁の数字
-    token { "tok_#{Faker::Alphanumeric.alphanumeric(number: 10)}" } # ランダムなtokenを設定
+    credit_card { '1234567890123456' }
+    token { "tok_#{Faker::Alphanumeric.alphanumeric(number: 10)}" } 
   end
 end
