6763982c28358d2d2c8d0e9b053e5054eef882dd
takamasa-tech <g1012075@icloud.com>
2024-09-18 19:56:10 +0900

    47

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderForm, type: :model do
   before do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
-    @order_form = FactoryBot.build(:order_form)
+    @order_form = FactoryBot.build(:order_form, user_id: user.id, item_id: item.id)
   end
 
   context '登録できる場合' do
