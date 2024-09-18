f571ff9230a154366f19a35836df2cb98fc0a866
takamasa-tech <g1012075@icloud.com>
2024-09-18 19:54:57 +0900

    46

--- spec/factories/order_form.rb
+++ spec/factories/order_form.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     addresses { '青山1-1-1' }
     house_number { '青山マンション101' }
     phone_number { '09012345678' }
-    user_id { 1 }
-    item_id { 1 }
     token { 'tok_abcdefghijk00000000000000000' }
     prefecture_id { 2 }
   end
--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
     @order_form = FactoryBot.build(:order_form)
   end
 
