c2dbd7834ab46e02dae4fbde29a4c03eb0119740
takamasa-tech <g1012075@icloud.com>
2024-09-19 16:14:23 +0900

    10

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,22 +1,10 @@
-const price = () => {
-
-  const priceInput = document.getElementById('item_price');
-  if (priceInput) {
-  priceInput.addEventListener('keypress', () => {
-    const inputValue = priceInput.value;
-    
-    const addTaxPrice = Math.floor(inputValue * 0.1);
-    const addTaxPriceElement = document.getElementById('add-tax-price');
-    addTaxPriceElement.innerHTML = addTaxPrice;
-
-    const salesProfit = Math.floor(inputValue * 0.9);
-    const salesProfitElement = document.getElementById('profit');
-    salesProfitElement.innerHTML = salesProfit;
+window.addEventListener('load', () => {
+  const price = document.getElementById('item-price');
+  const tax = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+  price.addEventListener('input', () => {
+    const value = price.value;
+    tax.innerHTML = Math.floor(value * 0.1);
+    profit.innerHTML = Math.floor(value * 0.9);
   });
-  }
-};
-
-window.addEventListener('turbo:load', price);
-window.addEventListener('turbo:render', price);
-window.addEventListener('DOMContentLoaded', price);
-
+});
