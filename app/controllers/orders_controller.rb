9f01d1d4cc6e96fd46cb997c9775e5e076f61015
takamasa-tech <g1012075@icloud.com>
2024-09-22 19:00:31 +0900

    43

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create, :edit]
   before_action :redirect_if_not_valid, only: [:index, :create]
-  before_action :set_payjp_public_key, only: [:index, :new, :create]
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
@@ -10,25 +9,30 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order_form = OrderForm.new(order_params)
-    if @order_form.valid?
+    @order = Order.new(order_params)
+    if @order.valid?
       pay_item
-      @order_form.process_order
-      redirect_to root_path
+      @order.save
+      return redirect_to root_path
     else
-      render :index
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
     end
   end
 
-
   private
+
+  def pay_item
+    Payjp.api_key =  ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 
   def set_item
    @item = Item.find_by(id: params[:item_id])
-  end
-
-  def set_payjp_public_key
-    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
 
@@ -38,14 +42,5 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :address, :house_number, :phone_number, :token).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
-  end
-
-  def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-     amount: Item.find(params[:item_id]).price,
-     card: order_params[:token],
-     currency: 'jpy'
-    )
   end
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,33 +1,36 @@
-const pay = () => {
-  const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) 
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
-
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-
-  const form = document.getElementById('charge-form')
-  form.addEventListener("submit", (e) => {
-    payjp.createToken(numberElement).then(function (response) {
-      if (response.error) {
-        console.log(gon.public_key)
-      } else {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
+window.addEventListener('DOMContentLoaded', () => {
+  const pay = () => {
+    if (window.gon) {
+    const publicKey = gon.public_key
+    const payjp = Payjp(publicKey);
+    const elements = payjp.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
+  
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+    const form = document.getElementById('charge-form')
+    form.addEventListener("submit", (e) => {
+      payjp.createToken(numberElement).then(function (response) {
+        if (response.error) {
+         // console.log(response.error.message);
+        } else {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.submit();
+      
+        e.preventDefault();
+        });
     });
-    e.preventDefault();
+  }
+  }
+  pay();
   });
-};
-
-window.addEventListener("turbo:load", pay);
