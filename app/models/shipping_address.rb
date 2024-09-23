e71dbb3f23d4b91f41e27885a8547a7572350f3f
takamasa-tech <g1012075@icloud.com>
2024-09-23 14:47:54 +0900

    71

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,10 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
+
+  attribute :postal_code, :string
+  attribute :prefecture_id, :integer
+  attribute :city, :string
+  attribute :house_number, :string
+  attribute :phone_number, :string
+  attribute :address, :string
 end
