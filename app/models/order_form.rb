61cbffe53f3bd46b958b3ea045d66c25c7f2f09e
takamasa-tech <g1012075@icloud.com>
2024-09-23 14:14:37 +0900

    ６６

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,7 +8,7 @@ class OrderForm
   validates :item_id
   validates :token
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture, numericality: { other_than: 1 }
   validates :city
   validates :address
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }
